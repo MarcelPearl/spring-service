7cc020fdebceaac4dd141c1c8ca68504cb99164d
Marcella_2706 <saumyasood6@gmail.com>
2025-06-18 20:56:17 +0530

    new:env-created

--- backend/src/main/java/com/marcella/backend/BackendApplication.java
+++ backend/src/main/java/com/marcella/backend/BackendApplication.java
@@ -1,11 +1,17 @@
 package com.marcella.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
+	static{
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
